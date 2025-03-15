0b7df6d6e2ee8faff63a1aa930dbd4ae0b1a8c1e
Johnathan Bill <59296885+Johnathan-Bill@users.noreply.github.com>
2025-03-15 17:55:07 -0400

    added remote testing files

--- script.js
+++ script.js
@@ -1,39 +1,73 @@
-
-function onStart(event)
-{
-	document.querySelectorAll("nav a").forEach((link) => {
-	link.addEventListener("click", function(event)
-	{
-		event.preventDefault();
-		loadpage(this.getAttribute("href"))
-	});
-  });
-}
-
-async function loadpage(page)
-{
-	console.log(page)
-	let parser = new DOMParser();
-	window.history.pushState({}, "", page.replace(".html",""));
-	let route = routes[page] || routes[404];
-	console.log(route);
-	const html = await fetch(route).then((data) => data.text());
-	document.getElementById("main-content").innerHTML = parser.parseFromString(html, "text/html").getElementById("main-content").innerHTML
-}
+let parser = new DOMParser();
 
 const routes = {
-  404: "404",
-  "": "index.html",
-  "/": "index.html",
-  "/index": "index.html",
-  "/home": "index.html",
-  "/about": "index.html",
-  "projects.html": "projects.html",
-  "/projects.html": "projects.html",
-  "/projects": "/projects.html",
+  "": {
+    content: "",
+    file: "index.html",
+  },
+  "/": {
+    content: "",
+    file: "index.html",
+  },
+  "index": {
+    content: "",
+    file: "index.html",
+  },
+  "projects": {
+    content: "",
+    file: "projects.html",
+  },
 };
-document.addEventListener("DOMContentLoaded",onStart)
 
-window.onpopstate = function (event) {
-  loadpage(window.location.pathname.replace(".html",""));
-};
+async function loadRoutes() {
+
+	for (const [path,{content,file}] of Object.entries(routes))
+	{
+			routes[path].content = parser.parseFromString
+			(await fetch(file).then((data) => data.text()),"text/html")
+			.getElementById("main-content").innerHTML;
+	}
+}
+
+function onStart()
+{
+	const query = window.matchMedia(
+  		'(prefers-reduced-motion: reduce)');
+	if(query.matches)
+	{
+		return;
+	}
+
+	document.querySelectorAll("nav a").forEach((link) =>
+	{
+
+		link.addEventListener("click", function(event)
+	{
+		event.preventDefault();
+		let path = this.getAttribute("href").replace(".html","");
+		if (path === "index" || path === "/") path = "";
+
+		console.log(path);
+		if (path === window.location.pathname.replace(".html", "").replace("/", "")) return;
+
+		
+      	window.history.pushState({}, "", `/${path}`);
+		loadContent(path);
+		
+
+	})
+	});
+}
+
+function loadContent(path)
+{
+	document.getElementById("main-content").innerHTML = routes[path].content;
+
+}
+
+loadRoutes();
+document.addEventListener("DOMContentLoaded",onStart)
+window.onpopstate = function (event)
+{
+	loadContent(window.location.pathname.replace( ".html","").replace('/',""))
+}
